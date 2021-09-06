9b2def31cf5a4524549174e8b5706a08a0f3629a
smontana30 <smontana@knights.ucf.edu>
2021-09-06 00:01:49 -0400

    timer code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [seconds, setSecond] = useState('00');
+  const [minute, setMinute] = useState('00');
+  const [isActive, setIsActive] = useState(false);
+  const [counter, setCounter] = useState(0);
+  
+  useEffect(() => {
+    let intervalId;
+    if (isActive) {
+      intervalId = setInterval(() => {
+        const secondCounter = counter % 60;
+        const minuteCounter = Math.floor(counter / 60)
+
+        const computedSecond = String(secondCounter).length === 1 ? `0${secondCounter}`: secondCounter;
+        const computedMinute = String(minuteCounter).length === 1 ? `0${minuteCounter}`: minuteCounter;
+      
+        setSecond(computedSecond);
+        setMinute(computedMinute);
+
+        setCounter(counter => counter + 1);
+      }, 1000)
+    }
+    return () => clearInterval(intervalId);
+  }, [isActive, counter])
+
+  const stopTimer = () => {
+    setIsActive(false);
+    setCounter(0);
+    setMinute('00');
+    setSecond('00');
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="time">
+          <span className='minutes'>{minute}</span>
+          <span>:</span>
+          <span className='seconds'>{seconds}</span>
+        </div>
+        <div className='settings'>
+          <button onClick={() => setIsActive(!isActive)} className='start'>{isActive ? 'Pause' : 'Start'}</button>
+          <button onClick={stopTimer} className='reset'>Reset</button>
+        </div>
     </div>
   );
 }
